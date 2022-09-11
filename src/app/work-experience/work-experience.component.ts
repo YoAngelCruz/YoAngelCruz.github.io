8f58437986ee8a0f5372c459f3da9a45caf7b1a5
YoAngelCruz <angelcruz.feb@gmail.com>
2022-09-10 23:42:06 -0500

    create work experience #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+    let work1 = {
+      fecha : "2016-2021",
+      ubicacion : "Cordoba, Ver",
+      puesto : "Socio",
+      empresa : "Sinergia SA de CV", 
+      logros : [
+          {descripcion: "Implementacion de nueva BD"},
+          {descripcion: "Nueva estrategia de venta"},
+
+      ]
+    };
+    let work2 = {
+      fecha : "2022-2023",
+      ubicacion : "MB, Fortin",
+      puesto : "SalesMan",
+      empresa : "Burguis los Ciegs", 
+      logros : [
+        {descripcion: "Actualizacion del hardware a nivel departamento"},
+        {descripcion: "Implementacion de metodologia SCRUM"},
+
+    ]
+    };
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
+
+
+
+
   }
 
 }
