4a7b20d7fe111b8c95fa3d40c21c234c0ce362fd
angel <angelcruz.feb@gmail.com>
2023-09-08 08:34:45 -0600

    add certificates

--- src/app/achievements-and-certificates/achievements-and-certificates.component.ts
+++ src/app/achievements-and-certificates/achievements-and-certificates.component.ts
@@ -31,13 +31,22 @@ export class AchievementsAndCertificatesComponent implements OnInit {
       logro:"Python Curse in Kaggle",
       fecha:"2023",
       certificado : [
-        {descripcion:"https://github.com/YoAngelCruz/curse_python/blob/main/Angel%20Cruz%20-%20Python.png"},
+        {descripcion:"https://github.com/YoAngelCruz/curses/blob/main/pyhon/Angel%20Cruz%20-%20Python.png"},
+      ]
+    };
+    let cert4 = {
+      logro:"Python Curse in Kaggle",
+      fecha:"2023",
+      certificado : [
+        {descripcion:"https://github.com/YoAngelCruz/curses/blob/main/pandas/pandas_certificate.png"},
       ]
     };
 
     this.certificates.push(cert1);
     this.certificates.push(cert2);
     this.certificates.push(cert3);
+    this.certificates.push(cert4);
+
 
   }
 
